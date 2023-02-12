f3ceb8db34780b911057875cfc916597df147179
DavidAlbUwU <117684736+DavidAlbUwU@users.noreply.github.com>
2023-02-12 16:40:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var image = ["https://i.postimg.cc/fRYbzdNt/IMG-7073.jpg"];
+    var name = ["David","Gilvane","Gaby","Elisângela"]
+var i = 0; () {
+    i++;
+    var numberOfMemberInArray = 5
+    if(i > numbersOfMemberInArray){
+        i = 0;
+    }
+    document.getElementById(familyImage).innerHTML = htmldata;
+}  
